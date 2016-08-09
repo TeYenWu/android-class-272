dad7c9a4d49994474ddef397d04c51104d82e590
Wuteyan <killerw74@gmail.com>
2016-08-09 11:28:12 +0800

    add editText and radioGroup

--- SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
@@ -3,11 +3,17 @@ package com.example.user.simpleui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView textView;
+    EditText editText;
+    RadioGroup radioGroup;
+
+    String drink = "Black Tea";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,11 +21,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textView = (TextView)findViewById(R.id.textView);
+        editText = (EditText)findViewById(R.id.editText);
+        radioGroup = (RadioGroup)findViewById(R.id.radioGroup);
 
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if(checkedId == R.id.blackTeaRadioButton)
+                {
+                    drink = "Black Tea";
+                }
+                else if (checkedId == R.id.greenTeaRadioButton)
+                {
+                    drink = "Green Tea";
+                }
+            }
+        });
     }
 
     public void click(View view)
     {
-        textView.setText("Hello TextView!!!");
+        String text = editText.getText().toString();
+        text = text + "  Order: " + drink;
+        textView.setText(text);
     }
 }
