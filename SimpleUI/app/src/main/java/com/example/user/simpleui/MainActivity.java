11aa1d656e25a6d64cf60f72a911e9681da223c4
Wuteyan <killerw74@gmail.com>
2016-08-10 10:31:52 +0800

    add spinner

--- SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
@@ -7,9 +7,12 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,8 +20,11 @@ public class MainActivity extends AppCompatActivity {
     EditText editText;
     RadioGroup radioGroup;
     ListView listView;
+    Spinner spinner;
 
     String drink = "Black Tea";
+
+    List<String> data = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
         editText = (EditText)findViewById(R.id.editText);
         radioGroup = (RadioGroup)findViewById(R.id.radioGroup);
         listView = (ListView)findViewById(R.id.listView);
+        spinner = (Spinner)findViewById(R.id.spinner);
 
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
@@ -45,14 +52,22 @@ public class MainActivity extends AppCompatActivity {
         });
 
         setupListView();
+        setupSpinner();
     }
 
     private void setupListView()
     {
-        String[] data = new String[]{"1","2","3","4","5","6","7","8"};
+//        String[] data = new String[]{"1","2","3","4","5","6","7","8"};
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, data);
         listView.setAdapter(adapter);
+    }
+
+    private void setupSpinner()
+    {
+        String[] storeInfo = getResources().getStringArray(R.array.storeInfo);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, storeInfo);
+        spinner.setAdapter(adapter);
     }
 
     public void click(View view)
@@ -60,5 +75,10 @@ public class MainActivity extends AppCompatActivity {
         String text = editText.getText().toString();
         text = text + "  Order: " + drink;
         textView.setText(text);
+
+        editText.setText("");
+
+        data.add(text);
+        setupListView();
     }
 }
