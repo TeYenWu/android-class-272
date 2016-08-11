c7b4a681e90583c4f79ddf91f85bf03a5a759670
Wuteyan <killerw74@gmail.com>
2016-08-11 11:55:24 +0800

    finish Drink Menu List

---
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/DrinkAdapter.java
@@ -0,0 +1,75 @@
+package com.example.user.simpleui;
+
+import android.content.Context;
+import android.media.Image;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by user on 2016/8/11.
+ */
+public class DrinkAdapter extends BaseAdapter {
+
+    List<Drink> drinkList;
+    LayoutInflater inflater;
+
+    public DrinkAdapter(Context context, List<Drink> drinks)
+    {
+        this.drinkList = drinks;
+        inflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public int getCount() {
+        return drinkList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return drinkList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Holder holder;
+        if(convertView == null)
+        {
+            convertView = inflater.inflate(R.layout.listview_drink_item, null);
+            holder = new Holder();
+            holder.drinkNameTextView = (TextView)convertView.findViewById(R.id.drinkNameTextView);
+            holder.lPriceTextView = (TextView)convertView.findViewById(R.id.lPriceTextView);
+            holder.mPriceTextView = (TextView)convertView.findViewById(R.id.mPriceTextView);
+            holder.imageView = (ImageView)convertView.findViewById(R.id.imageView);
+            convertView.setTag(holder);
+        }
+        else
+        {
+            holder = (Holder)convertView.getTag();
+        }
+
+        Drink drink = drinkList.get(position);
+        holder.drinkNameTextView.setText(drink.name);
+        holder.mPriceTextView.setText(String.valueOf(drink.mPrice));
+        holder.lPriceTextView.setText(String.valueOf(drink.lPrice));
+        holder.imageView.setImageResource(drink.imageId);
+        return convertView;
+    }
+
+    class Holder{
+        TextView drinkNameTextView;
+        TextView mPriceTextView;
+        TextView lPriceTextView;
+        ImageView imageView;
+    }
+}
