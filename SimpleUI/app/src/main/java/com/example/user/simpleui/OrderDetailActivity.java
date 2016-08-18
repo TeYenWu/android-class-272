6c84e1760c1d8672bcb9ff832fc684d3d5a146ce
Wuteyan <killerw74@gmail.com>
2016-08-18 11:28:43 +0800

    set order data on order detail layout

--- SimpleUI/app/src/main/java/com/example/user/simpleui/OrderDetailActivity.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/OrderDetailActivity.java
@@ -1,7 +1,9 @@
 package com.example.user.simpleui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class OrderDetailActivity extends AppCompatActivity {
 
@@ -9,5 +11,27 @@ public class OrderDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_detail);
+
+        TextView noteTextView = (TextView)findViewById(R.id.noteTextView);
+        TextView storeInfoTextView = (TextView)findViewById(R.id.storeInfoTextView);
+        TextView drinkOrderResultsTextView = (TextView)findViewById(R.id.drinkOrderResultsTextView);
+        TextView latlngTextView = (TextView)findViewById(R.id.latLngTextView);
+
+        Intent intent = getIntent();
+        Order order = intent.getParcelableExtra("order");
+        noteTextView.setText(order.getNote());
+        storeInfoTextView.setText(order.getStoreInfo());
+        String resultText = "";
+        for (DrinkOrder drinkOrder: order.getDrinkOrderList())
+        {
+            String lNumber = String.valueOf(drinkOrder.getlNumber());
+            String mNumber = String.valueOf(drinkOrder.getmNumber());
+            String drinkName = drinkOrder.getDrink().getName();
+            resultText+=drinkName + "  M:" + mNumber + "  L:" + lNumber + "\n";
+        }
+        drinkOrderResultsTextView.setText(resultText);
+
+
     }
+
 }
